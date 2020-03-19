40a0919f6fc0c3c28eb414cb9bd3890320f7e02a
josecalvano1546 <josecalvano1546@gmail.com>
2020-03-19 11:03:32 -0300

    formulario

--- src/app/aadmin/components/carga-producto/carga-producto.component.ts
+++ src/app/aadmin/components/carga-producto/carga-producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-carga-producto',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CargaProductoComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { 
+    this.buildForm();
+  }
 
   ngOnInit(): void {
+    
   }
+  private  buildForm(){
+    this.form = this.formBuilder.group({
+      id: ['',[Validators.required]],
+      title: ['',[Validators.required]],
+      price: ['',[Validators.required]],
+      image: [],
+      description: ['',[Validators.required]],
+  });
+}
 
 }
